96d8ce3c72d597e946f74b909adfb3a1d86ef4a2
Artem <blbo@yandex.ru>
2024-01-16 13:35:07 +0400

    0.0.2
    Добавил необходимые файлы

---
+++ scripts.mjs
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch';
+
+const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'c3e04b1af1msh53411ea8b6accd1p12cab9jsnaf849555125c',
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+  }
+};
+
+try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result); //можно выбрать только интересующую даныне (result.location.name)
+
+} catch (error) {
+        console.error(error);
+}
