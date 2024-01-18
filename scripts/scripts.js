218d39cce22f920237387a8af5cacc3a5206f612
Artem <blbo@yandex.ru>
2024-01-18 15:39:35 +0400

    0.1
    Первая работая версия. Города всё ещё нужно набирать на латинице.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,42 +1,50 @@
 //поиск по городу
 let formSearch = document.querySelector('#form__search'); //поле ввода
 const formButton = document.querySelector('#form__button'); //кнопка поиска
+
 //переменные json запроса
 let url = '';
 let response;
 let result;
 
+//функция поиска по городу
 formButton.onclick = function() {
   url = `http://api.weatherapi.com/v1/current.json?key=75b9c736df53403297a115728241601%20&q=${formSearch.value}&aqi=no`;
   start();
 }
 
-async function start() {
-  response = await fetch(url);
-  result = await response.json();
+  //температура и ощущается как 
+function temperatureFun() {
+  //температура
+  const indicatorsTemperature  = document.querySelector('.indicators__temperature');
+  indicatorsTemperature.insertAdjacentHTML('beforebegin',`${Math.round((JSON.stringify(result.current.temp_c)))}`);
+  //температура ощущается как 
+  const indicatorsFeeling  = document.querySelector('.indicators__feeling');
+  indicatorsFeeling.insertAdjacentHTML('beforebegin',`${Math.round((JSON.stringify(result.current.feelslike_c)))}`);
+}
+
 //город
+function cityFun() {
   let dateCity = document.querySelector('.date__city');
   dateCity.insertAdjacentHTML('beforebegin',`${(JSON.stringify(result.location.name).slice(1,-1)).toUpperCase()}`);
-
-//температура
-const indicatorsTemperature  = document.querySelector('.indicators__temperature');
-indicatorsTemperature.insertAdjacentHTML('beforebegin',`${Math.round((JSON.stringify(result.current.temp_c)))}`);
-
-//ощущается как
-const indicatorsFeeling  = document.querySelector('.indicators__feeling');
-indicatorsFeeling.insertAdjacentHTML('beforebegin',`${Math.round((JSON.stringify(result.current.feelslike_c)))}`);
+}
 
 //влажность
-const indicatorsHumidity = document.querySelector('.indicators__humidity-indicator');
-indicatorsHumidity.insertAdjacentHTML('beforebegin',`${(JSON.stringify(result.current.humidity))}`);
+function humidityFun(){
+  const indicatorsHumidity = document.querySelector('.indicators__humidity-indicator');
+  indicatorsHumidity.insertAdjacentHTML('beforebegin',`${(JSON.stringify(result.current.humidity))}`); 
+}
 
 //давление
-const indicatorsPressure = document.querySelector('.indicators__pressure-indicator');
-indicatorsPressure.insertAdjacentHTML('beforebegin',`${Math.round(JSON.stringify(result.current.pressure_mb) * 0.750063755419211)}`);
+function pressureFun(){
+  const indicatorsPressure = document.querySelector('.indicators__pressure-indicator');
+  indicatorsPressure.insertAdjacentHTML('beforebegin',`${Math.round(JSON.stringify(result.current.pressure_mb) * 0.750063755419211)}`);
+}
 
 //ветер
-const indicatorsWind = document.querySelector('.indicators__wind-indicator');
-indicatorsWind.insertAdjacentHTML('beforebegin',`${Math.round(JSON.stringify(result.current.wind_kph) * 0.27777777778)}`);
+function windFun(){
+  const indicatorsWind = document.querySelector('.indicators__wind-indicator');
+  indicatorsWind.insertAdjacentHTML('beforebegin',`${Math.round(JSON.stringify(result.current.wind_kph) * 0.27777777778)}`);
 
 //направление ветра
 const directionWind = document.querySelector('.indicators__directionWind-indicator');
@@ -96,8 +104,10 @@ function directionWindFunc(direction) {
       }
       return direction;
 }
+}
 
-//погода
+// состояние погоды
+function weatherFun() {
 const indicatorsWeather = document.querySelector('.indicators__weather-title');
 const weather = '';
 indicatorsWeather.insertAdjacentHTML('beforebegin',`${WindFunc()}`);
@@ -259,16 +269,22 @@ const WeatherImgDay = result.current.condition.icon.slice(35, -8); //день и
 
 indicatorsWeatherImg.style.background = (WeatherImgDay === 'day') ? `url(../img/weather/day/${WeatherImg}.png) no-repeat center` 
 : `url(../img/weather/night/${WeatherImg}.png) no-repeat center`;
-
-
 }
 
-//-------------- Блок текущая дата-----------------------
-//город
-// const dateCity = document.querySelector('.date__city');
-// dateCity.insertAdjacentHTML('beforebegin',`${(JSON.stringify(result.location.name).slice(1,-1)).toUpperCase()}`);
+//функция запуска после поиска по городу
+async function start() {
+  response = await fetch(url);
+  result = await response.json();
 
-//день недели
+  temperatureFun();
+  cityFun();
+  humidityFun();
+  pressureFun();
+  windFun();
+  weatherFun();
+}
+
+//-----------дата и время-----------------
 const newDate = new Date();
 
 const days =["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
@@ -291,6 +307,7 @@ function checkTime(i) {
     }
     return i;
   }
+  
   function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -303,12 +320,6 @@ function checkTime(i) {
   }
   startTime();
 
-//----- Блок индикаторы 
-
-
-
 //{"location":{"name":"Togliatti","region":"Samara","country":"Russia","lat":53.52,"lon":49.41,"tz_id":"Europe/Samara","localtime_epoch":1705464317,"localtime":"2024-01-17 8:05"},"current":{"last_updated_epoch":1705464000,"last_updated":"2024-01-17 08:00","temp_c":-3.7,"temp_f":25.3,"is_day":0,"condition":{"text":"Blizzard","icon":"//cdn.weatherapi.com/weather/64x64/night/230.png","code":1117},"wind_mph":22.8,"wind_kph":36.7,"wind_degree":187,"wind_dir":"S","pressure_mb":1005,"pressure_in":29.66,"precip_mm":0.27,"precip_in":0.01,"humidity":93,"cloud":100,"feelslike_c":-12.1,"feelslike_f":10.2,"vis_km":0,"vis_miles":0,"uv":1,"gust_mph":35.1,"gust_kph":56.4}}
   
-  //добавить поиск по городу
-
-//база городов на английском
+//база городов на английском - изменить на русский
