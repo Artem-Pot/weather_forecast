c596d46216b7624e88265c44ef4ae1b7a137d62f
Artem <blbo@yandex.ru>
2024-01-19 16:32:06 +0400

    0.1.4
    Добавлен русский язык

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,25 +6,43 @@ import { pressureFun } from "./modules/pressure.js"; //давление
 import { weatherFun } from "./modules/weather.js"; //погода
 import { startTime, startDate } from "./modules/date.js"; //дата и время
 
+//проба запускать при загрузке страницы инфу по Москве
+// function test() {
+//   // let url = `http://api.weatherapi.com/v1/current.json?key=75b9c736df53403297a115728241601%20&q=Москва&aqi=no&lang=ru`;
+//   let url = ``;
+//   let response;
+//   let result;
+//   async function requestJson2() {
+//     url = `http://api.weatherapi.com/v1/current.json?key=75b9c736df53403297a115728241601%20&q=Москва&aqi=no&lang=ru`;
+//     response = await fetch(url);
+//     result = await response.json();
+//   }
+
+//   requestJson2();
+//   setTimeout(start, 400);
+// }
+
+// test();
+
 //поиск по городу
 const formSearch = document.querySelector('#form__search'); //поле ввода
 const formButton = document.querySelector('#form__button'); //кнопка поиска
 
-let url = ``;
+let url = `http://api.weatherapi.com/v1/current.json?key=75b9c736df53403297a115728241601%20&q=Москва&aqi=no&lang=ru`;
 let response;
 let result;
 
-async function start2() {
+async function requestJson() {
   response = await fetch(url);
   result = await response.json();
 }
 
 //функция поиска по городу
 formButton.onclick = function() {
-  url = `http://api.weatherapi.com/v1/current.json?key=75b9c736df53403297a115728241601%20&q=${formSearch.value}&aqi=no`;
-  start2();
-  setTimeout(start, 100);
-  console.log(formSearch.value);
+  url = `http://api.weatherapi.com/v1/current.json?key=75b9c736df53403297a115728241601%20&q=${formSearch.value}&aqi=no&lang=ru`;
+  requestJson();
+  setTimeout(start, 400);
+  console.log(url);
 }
 
 //функция запуска после поиска по городу
@@ -35,17 +53,17 @@ function start() {
   pressureFun();
   windFun();
   weatherFun();
+  console.log(JSON.stringify(result));
 }
 
 export { result };
 
-//{"location":{"name":"Togliatti","region":"Samara","country":"Russia","lat":53.52,"lon":49.41,"tz_id":"Europe/Samara","localtime_epoch":1705464317,"localtime":"2024-01-17 8:05"},"current":{"last_updated_epoch":1705464000,"last_updated":"2024-01-17 08:00","temp_c":-3.7,"temp_f":25.3,"is_day":0,"condition":{"text":"Blizzard","icon":"//cdn.weatherapi.com/weather/64x64/night/230.png","code":1117},"wind_mph":22.8,"wind_kph":36.7,"wind_degree":187,"wind_dir":"S","pressure_mb":1005,"pressure_in":29.66,"precip_mm":0.27,"precip_in":0.01,"humidity":93,"cloud":100,"feelslike_c":-12.1,"feelslike_f":10.2,"vis_km":0,"vis_miles":0,"uv":1,"gust_mph":35.1,"gust_kph":56.4}}
-  
-
-//база городов на английском - изменить на русский
+//добавить чтобы автоматически загружалась погода по Москве 
 //ключ api перенести в отдельный модуль
-// перенести все функции в отдельные модули
-//добавить восход и заход солнца
+//добавить время восход и заход солнца
 //добавить порыв ветра
-//убрать баш при повторном нажатии на кнопку поиска. Просходит добавление параметров
+//убрать баг при повторном нажатии на кнопку поиска. Происходит добавление параметров
+//добавить историю на 5 дней вперёд
+
+//pug -w pug/index.pug -o ./ -P
 
